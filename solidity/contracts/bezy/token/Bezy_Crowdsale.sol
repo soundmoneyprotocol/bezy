073326634bca8c2178b4461c8013fbf7c3d05f20
soundmoneyprotocol <casmirconsults@gmail.com>
2024-12-14 14:47:05 -0500

    UPDATED: Bezy_Crowdsale.sol

--- solidity/contracts/bezy/token/Bezy_Crowdsale.sol
+++ solidity/contracts/bezy/token/Bezy_Crowdsale.sol
@@ -7,15 +7,31 @@ import "@openzeppelin/contracts-ethereum-package/contracts/crowdsale/emission/Mi
 
 contract BezyCrowdsale is Initializable, Crowdsale, MintedCrowdsale {
 
-    function initialize(
-        uint256 rate,    
-        // rate in TKNbits
-        address payable wallet,
-        IERC20 token
-    ) public initializer {
-        Crowdsale.initialize(rate, wallet, token);
+    constructor(
+        uint256 rate,            // rate, in TKNbits
+        address payable wallet,  // wallet to send Ether
+        IERC20 token,            // the token
+        uint256 openingTime,     // opening time in unix epoch seconds
+        uint256 closingTime      // closing time in unix epoch seconds
+    )
+        PostDeliveryCrowdsale()
+        TimedCrowdsale(openingTime, closingTime)
+        Crowdsale(rate, wallet, token)
+        public
+    {
+        // nice! this Crowdsale will keep all of the tokens until the end of the crowdsale
+        // and then users can `withdrawTokens()` to get the tokens they're owed
+        function initialize(
+            uint256 rate,    
+            // rate in TKNbits
+            address payable wallet,
+            IERC20 token
+        ) public initializer {
+            Crowdsale.initialize(rate, wallet, token);
+        }
     }
-}
+    }
+    
 
 
 
