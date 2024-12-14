f863df02908562bb953058175283baa7bd3d9447
soundmoneyprotocol <founder@soundmoneyprotocol.xyz>
2024-12-14 12:11:14 -0500

    Update Bezy.sol
    Added Minting initialization function.

--- solidity/contracts/bezy/token/Bezy.sol
+++ solidity/contracts/bezy/token/Bezy.sol
@@ -5,10 +5,14 @@ pragma solidity 0.8.17;
 import "@thesis/solidity-contracts/contracts/token/ERC20WithPermit.sol";
 import "@thesis/solidity-contracts/contracts/token/MisfundRecovery.sol";
 
-contract BEZY is T, ERC20WithPermit, MisfundRecovery {
+contract BEZY is T, ERC20WithPermit, MisfundRecovery, Initializable, ERC20, ERC20Detailed, ERC20Mintable {
     constructor() ERC20WithPermit("bzy", "BZY") {
         _mint(msg.sender, initialSupply);
        // add totalSupply here
         _totalSupply(uint256 250000000000000000000000000);
+    }
+function initialize(address minter) public initializer {
+        ERC20Detailed.initialize("Bezy", "BZY", 18);
+        ERC20Mintable.initialize(minter);
     }
 }
