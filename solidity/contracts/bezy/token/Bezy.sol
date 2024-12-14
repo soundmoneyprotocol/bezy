a3079e7939e18d7485082f7290002bf9e4aed7e7
soundmoneyprotocol <founder@soundmoneyprotocol.xyz>
2024-12-14 12:02:35 -0500

    Update Bezy.sol
    Added Total Supply 25,000,000 + 18 Zeros

--- solidity/contracts/bezy/token/Bezy.sol
+++ solidity/contracts/bezy/token/Bezy.sol
@@ -6,5 +6,9 @@ import "@thesis/solidity-contracts/contracts/token/ERC20WithPermit.sol";
 import "@thesis/solidity-contracts/contracts/token/MisfundRecovery.sol";
 
 contract BEZY is T, ERC20WithPermit, MisfundRecovery {
-    constructor() ERC20WithPermit("bzy", "BZY") {}
+    constructor() ERC20WithPermit("bzy", "BZY") {
+        _mint(msg.sender, initialSupply);
+       // add totalSupply here
+        _totalSupply(uint256 250000000000000000000000000000);
+    }
 }
