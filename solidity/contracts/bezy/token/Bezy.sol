96c3ab0ecea81ef1cca268b3840c397a05f0a472
soundmoneyprotocol <casmirconsults@gmail.com>
2024-12-15 19:24:37 -0500

    UPDATE: changed Token name from bzy to Bezy

--- solidity/contracts/bezy/token/Bezy.sol
+++ solidity/contracts/bezy/token/Bezy.sol
@@ -6,13 +6,13 @@ import "@thesis/solidity-contracts/contracts/token/ERC20WithPermit.sol";
 import "@thesis/solidity-contracts/contracts/token/MisfundRecovery.sol";
 
 contract BEZY is T, ERC20WithPermit, MisfundRecovery, Initializable, ERC20, ERC20Detailed, ERC20Mintable {
-    constructor() ERC20WithPermit("bzy", "BEZY", 18) {
+    constructor() ERC20WithPermit("Bezy", "BEZY", 18) {
         _mint(msg.sender, initialSupply);
        // add totalSupply here
         _totalSupply(uint256 250000000000000000000000000000);
     }
 function initialize(address minter) public initializer {
-        ERC20Detailed.initialize("Bezy", "BZY", 18);
+        ERC20Detailed.initialize("Bezy", "BEZY", 18);
         ERC20Mintable.initialize(minter);
     }
 }
